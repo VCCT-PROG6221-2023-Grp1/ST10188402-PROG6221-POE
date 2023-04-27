ba39563abfcfc9553a39b497f8f196fd10a62abd
EB <st10188402@vcconnect.edu.za>
2023-04-27 13:49:04 +0200

    Add project files.

---
+++ Classes/Ingredient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGPOE.Classes
+{
+    internal class Ingredient
+    {
+        string name;
+        double quantity;
+        string measurement_unit;
+
+        public Ingredient(string name, double quantity, string measurement_unit)
+        {
+            this.name = name;
+            this.quantity = quantity;
+            this.measurement_unit = measurement_unit;
+        }
+
+        /// <summary>
+        /// Gets Ingredient name
+        /// </summary>
+        /// <returns> name of ingredient </returns>
+        public string getName()
+        {
+            return name;
+        }
+
+        /// <summary>
+        /// Sets name of ingredient
+        /// </summary>
+        /// <param name="new_name"> name entered </param>
+        public void setName(string new_name)
+        {
+            name = new_name;
+        }
+
+        /// <summary>
+        /// Gets quantity of ingredient
+        /// </summary>
+        /// <returns> ingredient quantity</returns>
+        public double getQuantity()
+        {
+            return quantity;
+        }
+
+        /// <summary>
+        /// sets Quantity of Ingredient
+        /// </summary>
+        /// <param name="new_quantity"> quantity entered</param>
+        public void setQuantity(double new_quantity)
+        {
+            quantity = new_quantity;
+        }
+
+        /// <summary>
+        /// Gets unit of measurement of ingredient
+        /// </summary>
+        /// <returns></returns>
+        public string getMeasurementUnit()
+        {
+            return measurement_unit;
+        }
+
+        /// <summary>
+        /// Sets unit of measurement of ingredient
+        /// </summary>
+        /// <param name="new_measurement_unit"></param>
+        public void setMeasurementUnit(string new_measurement_unit)
+        {
+            measurement_unit = new_measurement_unit;
+        }
+    }
+}
+//______________________________---------------------------> End of File <---------------------------______________________________
---
+++ Classes/Worker.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGPOE.Classes
+{
+    internal class Worker
+    {
+        static int step_counter = 0; //step counter
+        string[] step_details; //details of a step
+        bool recipe_made = false; //if true recipe was made, if false no recipe
+        Ingredient[] ingredients; //array of ingredients
+
+        /// <summary>
+        /// Displays menu to 1.Create recipe, 2.Scale Recipe, 3.Reset Values, 4.Clear Recipe, 5. Show Recipe
+        /// --TO DO--
+        /// </summary>
+        public void Menu()
+        {
+        }
+
+        /// <summary>
+        /// Acquires user information recquired for recipe (Ingredients, amount of steps, step details)
+        /// --TO DO--
+        /// </summary>
+        public void CreateRecipe()
+        {
+        }
+
+        /// <summary>
+        /// Scales Recipe down or up according to user needs
+        /// --TO DO--
+        /// </summary>
+        public void ScaleRecipe()
+        {
+        }
+
+        /// <summary>
+        /// Resets value of recipe to original
+        /// --TO DO--
+        /// </summary>
+        public void ResetRecipe()
+        {
+        }
+
+        /// <summary>
+        /// Clears recipe data
+        /// --TO DO--
+        /// </summary>
+        public void ClearRecipe()
+        {
+        }
+
+        /// <summary>
+        /// Displays recipe
+        /// --TO DO--
+        /// </summary>
+        public void ShowRecipe()
+        {
+        }
+    
+}
+//______________________________---------------------------> End of File <---------------------------______________________________
